10a775e6953c75253293c8a54e9cb424a7d1b9d8
imranYRT <imran.khan.yrt@gmail.com>
2023-11-26 02:52:26 +0600

    add login functionality

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,11 @@
 import { useCallback, useState } from "react";
 import Input from "./components/Input";
 import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -12,6 +15,20 @@ const Auth = () => {
     setVariant((curentVariant) => (curentVariant === "login" ? "register" : "login"));
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
   const register = useCallback(async () => {
     try {
       await axios.post("/api/register", {
@@ -19,10 +36,11 @@ const Auth = () => {
         name,
         password,
       });
+      login();
     } catch (error) {
       console.log(error);
     }
-  }, [email, name, password]);
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
@@ -38,7 +56,7 @@ const Auth = () => {
               <Input label="Email" onChange={(event: any) => setEmail(event.target.value)} id="email" type="email" value={email} />
               <Input label="Password" onChange={(event: any) => setPassword(event.target.value)} id="password" type="password" value={password} />
             </div>
-            <button onClick={register} className="bg-red-600 text-white rounded-md w-full py-3 mt-10 hover:bg-red-700 transition">
+            <button onClick={variant === "login" ? login : register} className="bg-red-600 text-white rounded-md w-full py-3 mt-10 hover:bg-red-700 transition">
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
