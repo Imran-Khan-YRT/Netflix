04bfe802be6cda4ea0e7cf5f5aae3c8918cb7d9e
imranYRT <imran.khan.yrt@gmail.com>
2023-11-26 12:35:53 +0600

    add current user skeletonin UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "../hooks/useCurrentUser";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -17,9 +18,12 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <p className="text-white">Gmail: {user?.email}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout
       </button>
