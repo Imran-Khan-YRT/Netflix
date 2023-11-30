6f6d63c2100d4af55e9297ee4c26a0cef40dfad8
imranYRT <imran.khan.yrt@gmail.com>
2023-11-30 17:07:27 +0600

    add Navbar item in navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
-import useCurrentUser from "../hooks/useCurrentUser";
+import Navbar from "../components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -18,15 +18,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
-  const { data: user } = useCurrentUser();
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <p className="text-white">Gmail: {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout
-      </button>
+      <Navbar />
     </>
   );
 };
